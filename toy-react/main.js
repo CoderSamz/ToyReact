6b8d6fcff0c805ce3f4dbb03a82f37273ce2b311
CoderSamz <codersamz-dev@outlook.com>
2020-10-05 19:16:42 +0800

    JSX的原理和关键实现

--- toy-react/main.js
+++ toy-react/main.js
@@ -1,31 +1,40 @@
-for (let i of [1, 2, 3]) {
-    console.log(i);
+import {createElement, Component, render} from "./toy-react.js"
+
+class MyComponent extends Component{
+
+    render() {
+        return <div>
+            <h1>my component</h1>
+            {this.children}
+        </div>
+    }
 }
 
-function createElement(tagName, attributes, ...children) {
-    let e = document.createElement(tagName);
-    for (let p in attributes) {
-        e.setAttribute(p, attributes);
-    }
+render(<MyComponent id="a" class="c">
+    <div>abc</div>
+    <div></div>
+    <div></div>
+</MyComponent>, document.body);
 
-    for (let child of children) {
-        if (typeof child === "string") {
-            child = document.createTextNode(child);
-        }
-        e.appendChild(child);
-    }
-    return e;
 
-}
+// document.body.appendChild(<MyComponent id="a" class="c">
+//     <div>abc</div>
+//     <div></div>
+//     <div></div>
+// </MyComponent>
+// );
+
 
 // window.a = <div id="a" class="c">
 //     <div>abc</div>
 //     <div></div>
 //     <div></div>
 // </div>
-document.body.appendChild(<div id="a" class="c">
-    <div>abc</div>
-    <div></div>
-    <div></div>
-</div>
-);
+
+// document.body.appendChild(<div id="a" class="c">
+//     <div>abc</div>
+//     <div></div>
+//     <div></div>
+// </div>
+// );
+
