65e3b049566264e7c259e98134c672f68d13be2b
CoderSamz <codersamz-dev@outlook.com>
2020-10-07 19:12:18 +0800

    完成整个的虚拟DOM到实体DOM的更新

--- toy-react/toy-react.js
+++ toy-react/toy-react.js
@@ -22,6 +22,10 @@ export class Component {
         return this.render().vdom;
     }
 
+    get vchildren() {
+        return this.children.map(child => child.vdom);
+    }
+
     [RENDER_TO_DOM](range) {
         this._range = range;
         this.render()[RENDER_TO_DOM](range);
@@ -94,17 +98,41 @@ class ElementWrapper extends Component{
 */
 
     get vdom() {
-        return {
+        return this;
+        /*{
             type: this.type,
             props: this.pros,
             children: this.children.map(child => child.vdom)
-        }
+        }*/
     }
 
 
     [RENDER_TO_DOM](range) {
         range.deleteContents();
-        range.insertNode(this.root);
+
+        let root = document.createElement(this.type);
+
+        for (let name in this.props) {
+            let value = this.props[name];
+            if (name.match(/^on([\s\S]+)$/)) {
+                root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
+            } else {
+                if (name === "className") {
+                    root.setAttribute("class", value);
+                } else {
+                    root.setAttribute(name, value); 
+                }
+            }
+        }
+
+        for (let child  of this.children) {
+            let childRange = document.createRange();
+            childRange.setStart(root, root.childNodes.length);
+            childRange.setEnd(root, root.childNodes.length);
+            child[RENDER_TO_DOM](childRange);
+        }
+
+        range.insertNode(root);
     }
 }
 
@@ -112,15 +140,17 @@ class TextWrapper extends Component{
 
     constructor(content) {
         super(content);
+        this.type = "#text";
         this.content = content
         this.root = document.createTextNode(content);
     }
 
     get vdom() {
-        return {
+        return this;
+        /*{
             type: "#text",
             content: this.content
-        }
+        }*/
     }
 
     [RENDER_TO_DOM](range) {
